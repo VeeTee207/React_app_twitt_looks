235f2fa01faba5a25c8f74904d96dd224a9b5430
veetee207 <v.terehov207@gmail.com>
2021-03-20 10:17:51 -0400

    5. added on/ off for importance and likenes for each post-listitem , lesson 92 done

--- src/components/post-list-item/post-list-item.js
+++ src/components/post-list-item/post-list-item.js
@@ -2,23 +2,55 @@ import React, {Component} from "react";
 import './post-list-item.css';
 
 export default class PostListItem extends Component {
+    constructor(props) {
+        super();
+        this.state = {
+            important: false,
+            like: false,
+        };
+        this.onImportant = this.onImportant.bind(this);
+        this.onLike = this.onLike.bind(this);
+    }
+    
+    onImportant() {
+        this.setState(({ important }) => ({
+            important: !important,
+        }));
+    }
+
+    onLike() {
+        this.setState(({ like }) => ({
+            like: !like,
+        }));
+    }
+    
+
     render() {
-        const {label, important = false} = this.props;
+        const {label} = this.props;
+        const { important, like} = this.state;
 
         // sign important mark component to yellow 
 
         let classNames = 'app-list-item d-flex justify-content-between';
+
         if (important) {
             classNames += ' important';
-        }
+        };
+
+        if (like) {
+            classNames += ' like';
+        };
 
        return(
            <div className={classNames}>
-               <span className="app-list-item-label">
+               <span className="app-list-item-label"
+               onClick={this.onLike} >
                    {label}
                </span>
                <div className="d-flex justify-content-center align-items-center">
-                   <button className="btn-star btn-sm">
+                   <button className="btn-star btn-sm"
+                        onClick={this.onImportant}
+                   >
                        <i className="fa fa-star"></i>
                    </button>
                    <button className="btn-trash btn-sm">
